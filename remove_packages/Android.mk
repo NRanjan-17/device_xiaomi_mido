636c531902d3df23cbd50f6503b6cc8ed805aa34
NRanjan-17 <nalinishranjan05@gmail.com>
2022-05-24 00:03:54 +0530

    mido: import RemovePackages
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

---
+++ remove_packages/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt talkback arcore OPScreenRecord
+LOCAL_UNINSTALLABLE_MODULE := true 
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
