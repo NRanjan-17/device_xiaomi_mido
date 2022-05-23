8eb1ce95ba190f788be832b3d4809dffe9c440cc
GhostMaster69-dev <rathore6375@gmail.com>
2022-05-23 07:19:51 +0530

    mido: Set mido-user specific custom made private build description
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- evolution_mido.mk
+++ evolution_mido.mk
@@ -43,7 +43,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
+    PRIVATE_BUILD_DESC="mido-user $(PLATFORM_VERSION) $(BUILD_ID) $(shell date -u +%s) release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/mido/mido:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date -u +%H%M):user/release-keys"
