4e75588ae3d0ceac3b19f324fe08d7e1fc3e86dc
NRanjan-17 <nalinishranjan05@gmail.com>
2022-06-09 11:49:44 +0530

    mido: update build fp and desc to Pixel 6 Pro (raven)
    * Some apps in Play store shows device not supported so make it Pixel for spoofing it.
    
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- evolution_mido.mk
+++ evolution_mido.mk
@@ -43,7 +43,8 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP3A.220605.009.B1 8650216 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys"
+
