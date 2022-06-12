3306cd5348f248ec7c884d89212d55c4d2378df5
NRanjan-17 <nalinishranjan05@gmail.com>
2022-06-12 15:46:27 +0530

    mido: add more packages
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt talkback arcore OPScreenRecord
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt talkback arcore GoogleTTS Photos
 LOCAL_UNINSTALLABLE_MODULE := true 
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
