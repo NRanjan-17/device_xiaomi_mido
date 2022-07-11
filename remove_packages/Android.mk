f43795318899edcad49c91fa3e6746aa2c6e3167
NRanjan-17 <nalinishranjan05@gmail.com>
2022-07-11 21:50:44 +0530

    mido: reorder gapps
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube GoogleTTS SoundAmplifierPrebuilt Photos GoogleCamera arcore talkback FilesPrebuilt RecorderPrebuilt FM2 GoogleDialer Chrome PixelLiveWallpaperPrebuilt NexusLauncherRelease Phonesky
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt Photos GoogleCamera arcore talkback FM2 PixelLiveWallpaperPrebuilt NexusLauncherRelease
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
