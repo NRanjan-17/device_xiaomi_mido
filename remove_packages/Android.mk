e05316a0b5bd47895f67087e9a8367875a82ce0b
NRanjan-17 <nalinishranjan05@gmail.com>
2022-07-17 14:26:51 +0530

    mido: build NexusLauncherRelease
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt Photos GoogleCamera arcore talkback FM2 PixelLiveWallpaperPrebuilt NexusLauncherRelease
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt Photos GoogleCamera arcore talkback FM2 PixelLiveWallpaperPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
