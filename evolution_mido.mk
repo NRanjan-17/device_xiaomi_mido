bd61b388c4368b8e531e601fe905267a1549750c
strwbrry143 <alexfinhart@gmail.com>
2022-08-03 23:03:28 +0800

    mido: Bump to r20 raven build fingerprint
    * extracted from [1]
    
    [1]: https://github.com/wuxianlin/get_android_data/blob/master/data/android_images.csv#L47
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- evolution_mido.mk
+++ evolution_mido.mk
@@ -43,7 +43,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003.A1 8672226 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.004.A1 8847371 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.004.A1/8847371:user/release-keys"
