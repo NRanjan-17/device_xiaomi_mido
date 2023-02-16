97120451046e14aec5d4935145d028f5d0ade5be
NRanjan-17 <nalinishranjan05@gmail.com>
2023-02-16 15:01:23 +0530

    mido: don't build Camera2
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt talkback arcore Photos
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt talkback arcore Photos Camera2
 LOCAL_UNINSTALLABLE_MODULE := true 
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
