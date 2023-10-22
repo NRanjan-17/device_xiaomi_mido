8293935cddbca6c4dd72dd35aa1f5ddf3ce68272
NRanjan-17 <nalinishranjan05@gmail.com>
2023-10-22 14:14:39 +0530

    mido: adapt to DroidX-UI
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/droidx_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    droidx_mido-user \
+    droidx_mido-userdebug \
+    droidx_mido-eng
