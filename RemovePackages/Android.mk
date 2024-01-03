02b7eb59be334e26b55e08f4a3b80ab9f48c8ab8
NRanjan-17 <nalinishranjan05@gmail.com>
2024-01-03 23:04:25 +0530

    mido: Initialize for Evolution X 9.x
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,8 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \
-    NfcNci SecureElement DeviceAsWebcam
+LOCAL_OVERRIDES_PACKAGES := NfcNci SecureElement DeviceAsWebcam
+LOCAL_OVERRIDES_PACKAGES += Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES += AiWallpapers AICorePrebuilt
+LOCAL_OVERRIDES_PACKAGES += Camera2 Drive Maps Photos MaestroPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
--- lineage_mido.mk
+++ lineage_mido.mk
@@ -24,6 +24,16 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Custom stuff
+EVO_BUILD_TYPE := OFFICIAL
+TARGET_USES_LEGACY_BOOTANIMATION := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_MINI_GAPPS := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := lineage_mido
@@ -34,9 +44,3 @@ TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
