9bef3a742f8c4e3be89667a81784a3e5cac2a0ea
NRanjan-17 <nalinishranjan05@gmail.com>
2024-02-19 11:24:35 +0530

    mido: Don't build few apps

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,6 +4,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES += AiWallpapers AICorePrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true 
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
