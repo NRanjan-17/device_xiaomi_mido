7d3ecb6fe40b8b89d0bc59dfba2191f636931dad
Pranav Vashi <neobuddy89@gmail.com>
2024-07-20 17:51:45 +0200

    mido: Add missing stagefright libraries
    06-30 23:37:15.059  1475  1475 E vndksupport: Could not load libstagefrightdolby.so from default namespace: dlopen failed: library "libstagefright_softomx.so" not found: needed by /vendor/lib/libstagefrightdolby.so in namespace (default).
    06-30 23:37:15.060  1475  1475 E vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found.

--- device.mk
+++ device.mk
@@ -346,8 +346,15 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
+    libopus.vendor \
+    libstagefright_amrnb_common.vendor \
+    libstagefright_enc_common \
+    libstagefright_enc_common.vendor \
     libstagefrighthw \
-    libstagefright_enc_common
+    libstagefright_softomx_plugin.vendor \
+    libstagefright_softomx.vendor \
+    libvorbisidec.vendor \
+    libvpx.vendor
 
 # Power
 PRODUCT_PACKAGES += \
