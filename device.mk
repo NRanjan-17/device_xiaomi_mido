3fac2ef40b7e9313d68422e9afa2818e1eb67e4a
kamikaonashi <thorschi93@gmx.at>
2025-05-18 10:47:59 +0200

    mido: import powerhint.json
    taken and adapted to mido from
    https://github.com/MetoisTaken/device_xiaomi_msm8953-common_tiramisu/blob/13/power/configs/powerhint.json
    
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- device.mk
+++ device.mk
@@ -310,6 +310,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
