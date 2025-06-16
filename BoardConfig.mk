702eb79f759d79e9dbf186fec46f666baa06fde1
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    mido: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567
    Signed-off-by: Nalinish Ranjan <nalinishranjan05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,9 +55,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mido
 TARGET_KERNEL_VERSION := 4.9
 
-# ANT
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-
 # ART
 ART_BUILD_TARGET_NDEBUG := true
 ART_BUILD_TARGET_DEBUG := false
