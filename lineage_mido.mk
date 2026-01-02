cc226f86d718e9259ab527528e1c93ef8dd22c6a
Nalinish Ranjan <nalinishranjan05@gmail.com>
2026-01-02 20:03:28 +0530

    mido: Update quick tap and GApps settings in lineage_mido.mk
    Disabled quick tap support and switched from mini to pico GApps for the mido device configuration.

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -30,10 +30,10 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Custom stuff
-EVO_BUILD_TYPE := OFFICIAL
+EVO_BUILD_TYPE := Official
 TARGET_USES_LEGACY_BOOTANIMATION := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_QUICK_TAP := false
 TARGET_USES_PICO_GAPPS := true
 
 # Device identifier. This must come after all inclusions
