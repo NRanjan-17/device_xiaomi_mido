1718e7e26fd4908eff083db6c1bc752fed825ac9
kamikaonashi <thorschi93@gmx.at>
2026-02-11 22:24:05 +0800

    mido: use newer cgroups and task_profiles
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- device.mk
+++ device.mk
@@ -147,8 +147,8 @@ PRODUCT_PACKAGES += \
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # ConfigStore
 PRODUCT_PACKAGES += \
